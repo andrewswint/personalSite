92e45e1c67015c45a243c31429003672a38930e2
andrewswint <andrewswint1976@gmail.com>
2020-10-17 00:22:22 -0500

    starting my runners tool belt projects

---
+++ js/runnersTools.js
@@ -0,0 +1,6 @@
+function LengthConverter(valNum) {
+  document.getElementById("outputMiles").innerHTML=valNum*0.62137;
+}
+  function LengthConverter(valNum) {
+  document.getElementById("outputKilometers").innerHTML=valNum/0.62137;
+}
