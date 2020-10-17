c52f008c0a350c3945dc146498b79fd80f1fc277
andrewswint <andrewswint1976@gmail.com>
2020-10-17 14:04:55 -0500

    pace calculator addition

--- js/runnersTools.js
+++ js/runnersTools.js
@@ -1,9 +1,9 @@
 // Distance convertions
 function LengthConverter(valNum) {
-  document.getElementById("outputMiles").innerHTML=valNum*0.62137;
+  document.getElementById("outputMiles").innerHTML = "Miles: " + (valNum * 0.62137).toFixed(2);
 }
-  function LengthConverter(valNum) {
-  document.getElementById("outputKilometers").innerHTML=valNum/0.62137;
+  function LengthConverter2(valNum) {
+  document.getElementById("outputKilometers").innerHTML = "Kilometers: " + (valNum / 0.62137).toFixed(2);
 }
 
 
@@ -27,7 +27,61 @@ function dateDiff() {
 		document.getElementById("dateOutput").innerHTML= Math.floor(Difference_In_Days/7) + " weeks" + " " + Difference_In_Days.toFixed(0)%7 + " days";
 
 	} else {
-		document.getElementById("dateOutput").innerHTML=Difference_In_Days.toFixed(0) + " days";
+		document.getElementById("dateOutput").innerHTML="Time left " + Difference_In_Days.toFixed(0) + " days";
 	}
 }
+
+
+// Pace calculator
+var distanceInput = document.getElementById("distance"),
+	hoursInput = document.getElementById("hours"),
+	minutesInput = document.getElementById("minutes"),
+	secondsInput = document.getElementById("seconds"),
+	calculateBtn = document.getElementById("calculate_btn"),
+	paceText = document.getElementById("pace");
+
+calculateBtn.addEventListener("click", function() {
+	var miles = parseFloat(distanceInput.value),
+		hours = parseFloat(hoursInput.value),
+		minutes = parseFloat(minutesInput.value),
+		seconds = parseFloat(secondsInput.value);
+
+	if(isNaN(miles)) {
+		distanceInput.style.borderColor = "red";
+		return;
+	}
+	else {
+		distanceInput.style.borderColor = "initial";
+	}
+	if(isNaN(hours)) {
+		hours = 0;
+	}
+	else {
+		hoursInput.style.borderColor = "initial";
+	}
+	if(isNaN(minutes)) {
+		minutes = 0;
+	}
+	else {
+		minutesInput.style.borderColor = "initial";
+	}
+	if(isNaN(seconds)) {
+		seconds = 0
+	}
+	else {
+		secondsInput.style.borderColor = "initial";
+	}
+
+	var totalMinutes = hours * 60 + minutes + seconds / 60,
+		pace = totalMinutes / miles,
+		paceMinutes = Math.floor(pace),
+		paceSeconds = Math.round((pace - paceMinutes) * 60);
+
+	if(paceSeconds < 10) {
+		paceSeconds = "0" + paceSeconds;
+	}
+
+	document.getElementById("pace").innerHTML="You need to run " + paceMinutes + ":" + paceSeconds + " minutes per mile.";
+});
+
 
